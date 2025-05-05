c266fce015772a9c5a084ceef1f34cdf793dfa69
Julius Boakye <bbjulius4@gmail.com>
2025-05-05 08:24:40 +0000

    Remove build artifacts from Git

--- build/lib/django_quick_auth/apps.py
+++
@@ -1,6 +0,0 @@
-# django_quick_auth/apps.py
-from django.apps import AppConfig
-
-class DjangoQuickAuthConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_quick_auth'
